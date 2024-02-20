2b5314f1fdf71abda828e3464afddf2ed5e91330
Myp3a <murza8965@gmail.com>
2024-02-20 20:31:48 +0300

    feat: thorncaller

---
+++ cards/minions/thorncaller.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from cards.spells.blood_gem import BloodGem
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class Thorncaller(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 55
+        self.classes = [MinionClass.Quilboar]
+        self.level = 2
+        self.base_attack_value = 3
+        self.base_health_value = 2
+        self.hooks["deathrattle"].append(self.give_blood_gem)
+
+    def give_blood_gem(self) -> None:
+        self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
+        if self.triplet:
+            self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
