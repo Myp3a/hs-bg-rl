b302362418b62a084ca09d14e9052e7fb0f2ea64
Myp3a <murza8965@gmail.com>
2024-02-20 10:00:27 +0300

    feat: briarback bookie

---
+++ cards/minions/briarback_bookie.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from cards.spells.blood_gem import BloodGem
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class BriarbackBookie(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 28
+        self.classes = [MinionClass.Quilboar]
+        self.level = 2
+        self.base_attack_value = 3
+        self.base_health_value = 4
+        self.attack_value = self.base_attack_value
+        self.health_value = self.base_health_value
+        self.hooks["on_turn_end"].append(self.give_blood_gem)
+
+    def give_blood_gem(self) -> None:
+        if self.army.player.gold > 0:
+            self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
+            self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
+            if self.triplet:
+                self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
+                self.army.player.hand.add(BloodGem(self.army.player), len(self.army.player.hand))
