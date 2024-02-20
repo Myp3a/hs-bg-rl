c88f9cdf1d1511161d610e99bab408dc0c57dc24
Myp3a <murza8965@gmail.com>
2024-02-20 10:16:36 +0300

    feat: coldlight seer

---
+++ cards/minions/coldlight_seer.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class ColdlightSeer(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 31
+        self.classes = [MinionClass.Murloc]
+        self.level = 2
+        self.base_attack_value = 2
+        self.base_health_value = 3
+        self.attack_value = self.base_attack_value
+        self.health_value = self.base_health_value
+        self.hooks["battlecry"].append(self.boost_murloc)
+
+    def boost_murloc(self) -> None:
+        for minion in self.army.cards:
+            if MinionClass.Murloc in minion.classes:
+                if not minion is self:
+                    minion.health_value += 2
+                    if self.triplet:
+                        minion.health_value += 2
