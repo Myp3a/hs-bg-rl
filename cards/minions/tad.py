2851ca8c4f74fc579d03913408514bbdb18548e3
Myp3a <murza8965@gmail.com>
2024-02-20 20:29:19 +0300

    feat: tad

---
+++ cards/minions/tad.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class Tad(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 54
+        self.classes = [MinionClass.Murloc]
+        self.level = 2
+        self.base_attack_value = 2
+        self.base_health_value = 2
+        self.hooks["on_sell"].append(self.give_murloc)
+
+    def choose_and_give_murloc(self) -> None:
+        cards = [c for c in self.army.player.tavern.available_cards() if c.level <= self.army.player.level]
+        if len(cards) == 0:
+            return
+        if len(self.army.player.hand) == 10:
+            return
+        card = random.choice(cards)
+        self.army.player.tavern.buy(card)
+        self.army.player.hand.add(card, len(self.army.player.hand))
+
+    def give_murloc(self) -> None:
+        self.choose_and_give_murloc()
+        if self.triplet:
+            self.choose_and_give_murloc()
