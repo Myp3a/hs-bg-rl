008786d9bbe8e499772cda8f5d156df9852bb7a8
Myp3a <murza8965@gmail.com>
2024-02-21 12:30:35 +0300

    feat: accordotron

---
+++ cards/minions/accordotron.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class AccordOTron(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 60
+        self.classes = [MinionClass.Mech]
+        self.level = 3
+        self.base_attack_value = 3
+        self.base_health_value = 3
+        self.hooks["on_turn_start"].append(self.give_gold)
+
+    def give_gold(self) -> None:
+        self.army.player.gold += 1
