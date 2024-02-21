de8a317fb8f519976ca59650020a9da6b832a72e
Myp3a <murza8965@gmail.com>
2024-02-21 20:09:58 +0300

    feat: deflectobot

---
+++ cards/minions/deflectobot.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class DeflectOBot(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 64
+        self.classes = [MinionClass.Mech]
+        self.level = 3
+        self.base_attack_value = 3
+        self.base_health_value = 2
+        self.divine_shield = True
+        self.hooks["on_fight_start"].append(self.put_hook)
+        self.hooks["on_turn_start"].append(self.remove_hook)
+
+    def put_hook(self) -> None:
+        self.army.hooks["on_minion_summon"].append(self.get_shield)
+        
+    def remove_hook(self) -> None:
+        self.army.hooks["on_minion_summon"].remove(self.get_shield)
+
+    def get_shield(self, summoned: Minion) -> None:
+        if MinionClass.Mech in summoned.classes:
+            if self.triplet:
+                atk_boost = 4
+            else:
+                atk_boost = 2
+            self.divine_shield = True
+            self.attack_temp_boost += atk_boost
