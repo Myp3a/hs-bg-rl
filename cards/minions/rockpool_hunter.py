4eb2b4a0c5f1e97cb0907ccc5b8bc991454b2ce4
Myp3a <murza8965@gmail.com>
2024-02-21 21:08:44 +0300

    fix: wrong class

--- cards/minions/rockpool_hunter.py
+++ cards/minions/rockpool_hunter.py
@@ -12,7 +12,7 @@ class RockpoolHunter(Minion):
     def __init__(self, army: Army) -> None:
         super().__init__(army)
         self.minion_id = 14
-        self.classes = [MinionClass.Mech, MinionClass.Undead]
+        self.classes = [MinionClass.Murloc]
         self.level = 1
         self.base_attack_value = 2
         self.base_health_value = 3
