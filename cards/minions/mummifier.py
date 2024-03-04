be9e136aea38c58bdd3099bf962bbcd2df4bbecc
Myp3a <murza8965@gmail.com>
2024-03-04 23:33:47 +0300

    feat: mummifier

---
+++ cards/minions/mummifier.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class Mummifier(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 79
+        self.classes = [MinionClass.Undead]
+        self.level = 3
+        self.base_attack_value = 5
+        self.base_health_value = 2
+        self.hooks["deathrattle"].append(self.give_rebirth)
+
+    def choose_and_give_rebirth(self):
+        targets = [t for t in self.army.cards if MinionClass.Undead in t.classes and not t.rebirth]
+        if len(targets) == 0:
+            return
+        target = random.choice(targets)
+        target.rebirth = True
+        target.reborn = False
+
+    def give_rebirth(self):
+        if self.triplet:
+            self.choose_and_give_rebirth()
+        self.choose_and_give_rebirth()
