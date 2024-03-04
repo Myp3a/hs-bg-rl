c1b1c1188c7ef0bbdfdf4db0fdc7d0bc56c07f0a
Myp3a <murza8965@gmail.com>
2024-03-05 00:29:01 +0300

    feat: replicating menace

---
+++ cards/minions/replicating_menace.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from .microbot import Microbot
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class ReplicatingMenace(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 87
+        self.classes = [MinionClass.Mech]
+        self.level = 3
+        self.base_attack_value = 3
+        self.base_health_value = 2
+        self.magnetic = True
+        self.hooks["deathrattle"].append(self.summon_microbots)
+
+    def summon_microbots(self, position):
+        for _ in range(3):
+            mb = Microbot(self.army)
+            if self.triplet:
+                mb.triplet = True
+            self.army.add(mb, position)
