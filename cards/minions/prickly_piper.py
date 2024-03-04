14485f1fdb1e499344f648a59fcb8167977573c0
Myp3a <murza8965@gmail.com>
2024-03-05 00:16:13 +0300

    feat: prickly piper

---
+++ cards/minions/prickly_piper.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class PricklyPiper(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 85
+        self.classes = [MinionClass.Quilboar]
+        self.level = 3
+        self.base_attack_value = 5
+        self.base_health_value = 1
+        self.hooks["deathrattle"].append(self.boost_blood_gem_attack)
+
+    def boost_blood_gem_attack(self) -> None:
+        self.army.player.blood_gem_attack += 1
+        if self.triplet:
+            self.army.player.blood_gem_attack += 1
