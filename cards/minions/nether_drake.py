7ff803d07d916fd12d0339bd3b3f1710053f2ff8
Myp3a <murza8965@gmail.com>
2024-03-04 23:35:41 +0300

    feat: nether drake

---
+++ cards/minions/nether_drake.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class NetherDrake(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 80
+        self.classes = [MinionClass.Dragon]
+        self.level = 3
+        self.base_attack_value = 2
+        self.base_health_value = 4
+        self.hooks["on_turn_end"].append(self.boost_dragons)
+
+    def boost_dragons(self):
+        targets = [t for t in self.army.cards if MinionClass.Dragon in t.classes and not t is self]
+        if self.triplet:
+            atk_boost = 2
+        else:
+            atk_boost = 1
+        for t in targets:
+            t.attack_perm_boost += atk_boost
+            for hook in self.army.hooks["on_values_change_perm"]:
+                hook(t, atk_boost, 0)
