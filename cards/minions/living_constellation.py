0c6c1fe381d97f31d5627d5b789357794d62567e
Myp3a <murza8965@gmail.com>
2024-03-04 23:14:10 +0300

    feat: living constellation

---
+++ cards/minions/living_constellation.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class LivingConstellation(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 76
+        self.classes = []
+        self.level = 3
+        self.base_attack_value = 4
+        self.base_health_value = 2
+        self.hooks["battlecry"].append(self.boost_minion)
+
+    def boost_minion(self):
+        # TODO: make targeted battlecry mechanic
+        diff_classes = set()
+        for card in self.army.cards:
+            diff_classes |= set(card.classes)
+        if self.triplet:
+            atk_boost = 2
+            hlt_boost = 2
+        else:
+            atk_boost = 1
+            hlt_boost = 1
+        atk_boost *= len(diff_classes)
+        hlt_boost *= len(diff_classes)
+        targets = [t for t in self.army.cards if not t is self]
+        if len(targets) == 0:
+            return
+        target = random.choice(targets)
+        target.attack_perm_boost += atk_boost
+        target.health_perm_boost += hlt_boost
+        for hook in self.army.hooks["on_values_change_perm"]:
+            hook(target, atk_boost, hlt_boost)
