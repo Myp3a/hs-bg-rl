0f166dec5624e57eac4ac81262d9c27fcdc069ff
Myp3a <murza8965@gmail.com>
2024-03-05 15:57:59 +0300

    feat: saloon dancer

---
+++ cards/minions/saloon_dancer.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class SaloonDancer(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 126
+        self.classes = []
+        self.level = 4
+        self.base_attack_value = 4
+        self.base_health_value = 5
+        self.taunt = True
+        # TODO: tavern spells
