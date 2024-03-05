6556d2732c6836112649b9af2523895b2b1285d4
Myp3a <murza8965@gmail.com>
2024-03-05 22:49:39 +0300

    feat: treasure seeker elise

---
+++ cards/minions/golden_monkey.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class GoldenMonkey(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 134
+        self.classes = []
+        self.level = 1
+        self.base_attack_value = 3
+        self.base_health_value = 3
+        self.taunt = True
---
+++ cards/minions/treasure_seeker_elise.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from .golden_monkey import GoldenMonkey
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class TreasureSeekerElise(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 133
+        self.classes = []
+        self.level = 4
+        self.base_attack_value = 5
+        self.base_health_value = 5
+        self.rolls_left = 5
+        self.hooks["on_roll"].append(self.roll)
+      
+    def roll(self):
+        self.rolls_left -= 1
+        if self.rolls_left == 0:
+            self.army.player.view.remove(random.choice(self.army.player.view))
+            gm = GoldenMonkey(None)
+            gm.triplet = True
+            self.army.player.view.append(gm)
+            self.rolls_left = 5
