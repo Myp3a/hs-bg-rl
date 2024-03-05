f490f7117ed16d68b5c8c4d28efcfb8b624819c2
Myp3a <murza8965@gmail.com>
2024-03-05 15:39:53 +0300

    feat: razorgore the untamed

---
+++ cards/minions/razorgore_the_untamed.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class RazorgoreTheUntamed(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 123
+        self.classes = [MinionClass.Dragon]
+        self.level = 4
+        self.base_attack_value = 2
+        self.base_health_value = 4
+        self.hooks["on_turn_end"].append(self.boost_values)
+
+    def boost_values(self) -> None:
+        dragons = [d for d in self.army.cards if MinionClass.Dragon in d.classes and not d is self]
+        for _ in range(len(dragons)):
+            if self.triplet:
+                atk_boost = 2
+                hlt_boost = 2
+            else:
+                atk_boost = 1
+                hlt_boost = 1
+            self.attack_perm_boost += atk_boost
+            self.health_perm_boost += hlt_boost
+            for hook in self.army.hooks["on_values_change_perm"]:
+                hook(self, atk_boost, hlt_boost)
