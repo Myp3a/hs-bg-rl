75539db73a1a43e0821d309cd5b599ea692efc19
Myp3a <murza8965@gmail.com>
2024-03-05 15:21:46 +0300

    feat: outback smolderer

---
+++ cards/minions/outback_smolderer.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from cards.minions.smolderwing import Smolderwing
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class OutbackSmolderer(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 118
+        self.classes = [MinionClass.Dragon]
+        self.level = 4
+        self.base_attack_value = 4
+        self.base_health_value = 3
+        self.hooks["on_turn_end"].append(self.give_smolderwing)
+
+    def give_smolderwing(self):
+        if self.triplet:
+            sw = Smolderwing(self.army)
+            self.army.player.hand.add(sw, len(self.army.player.hand.cards))
+        sw = Smolderwing(self.army)
+        self.army.player.hand.add(sw, len(self.army.player.hand.cards))
---
+++ cards/minions/smolderwing.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import random
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class Smolderwing(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 119
+        self.classes = [MinionClass.Dragon]
+        self.level = 1
+        self.base_attack_value = 2
+        self.base_health_value = 1
+        self.hooks["battlecry"].append(self.boost_dragon)
+
+    def boost_dragon(self):
+        targets = [t for t in self.army.cards if MinionClass.Dragon in t.classes and not t is self]
+        if len(targets) == 0:
+            return
+        target = random.choice(targets)
+        if self.triplet:
+            atk_boost = 10
+        else:
+            atk_boost = 5
+        target.attack_perm_boost += atk_boost
+        for hook in self.army.hooks["on_values_change_perm"]:
+            hook(target, atk_boost, 0)
