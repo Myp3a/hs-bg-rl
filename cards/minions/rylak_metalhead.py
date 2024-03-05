e90e77263576249b6fc2d8cd21215df44fc6f857
Myp3a <murza8965@gmail.com>
2024-03-05 15:48:33 +0300

    feat: rylak metalhead

---
+++ cards/minions/rylak_metalhead.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class RylakMetalhead(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 125
+        self.classes = [MinionClass.Beast]
+        self.level = 4
+        self.base_attack_value = 3
+        self.base_health_value = 4
+        self.hooks["deathrattle"].append(self.trigger_battlecries)
+
+    def trigger_battlecries(self, position):
+        if position > 0:
+            for hook in self.army.cards[position - 1].hooks["battlecry"]:
+                hook()
+        if position < len(self.army.cards):
+            for hook in self.army.cards[position].hooks["battlecry"]:
+                hook()
