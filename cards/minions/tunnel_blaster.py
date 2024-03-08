1b30cb2793e27673d1f5c479234d9390a46b61c2
Myp3a <murza8965@gmail.com>
2024-03-08 22:17:33 +0300

    fix: tunnel blaster deathrattle triggers after removing army

--- cards/minions/tunnel_blaster.py
+++ cards/minions/tunnel_blaster.py
@@ -17,12 +17,17 @@ class TunnelBlaster(Minion):
         self.base_attack_value = 3
         self.base_health_value = 7
         self.taunt = True
+        self.friendly_army = None
         self.hooks["deathrattle"].append(self.damage_on_death)
+        self.hooks["on_fight_start"].append(self.set_friendly_army)
+
+    def set_friendly_army(self):
+        self.friendly_army = self.army
 
     def damage_all(self):
-        targets = [t for t in self.army.cards + self.enemy_army.cards]
+        targets = [t for t in self.friendly_army.cards + self.enemy_army.cards]
         for t in targets:
-            tnt = GameEntity(self.army)
+            tnt = GameEntity(self.friendly_army)
             tnt.base_attack_value = 3
             tnt.attack(t)
 
