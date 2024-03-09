1a6859004e6729a06140e68a1c38b7be5d00b07a
Myp3a <murza8965@gmail.com>
2024-03-09 15:48:38 +0300

    feat: ensorcelled fungus

---
+++ cards/minions/ensorcelled_fungus.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class EnsorcelledFungus(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 150
+        self.classes = []
+        self.level = 5
+        self.base_attack_value = 1
+        self.base_health_value = 5
+        # TODO: tavern spells
