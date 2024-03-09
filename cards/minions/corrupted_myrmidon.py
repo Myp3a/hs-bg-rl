e2a342b86f409150397a6d24cbedd621b865f2be
Myp3a <murza8965@gmail.com>
2024-03-09 15:35:48 +0300

    feat: corrupted myrmidon

---
+++ cards/minions/corrupted_myrmidon.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class CorruptedMyrmidon(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 147
+        self.classes = [MinionClass.Naga]
+        self.level = 5
+        self.base_attack_value = 3
+        self.base_health_value = 3
+        self.avenge_cntr = 3
+        self.hooks["on_fight_start"].append(self.boost_stats)
+
+    def boost_stats(self) -> None:
+        if self.triplet:
+            atk_bonus = self.attack_value * 2
+            hlt_bonus = self.health_value * 2
+        else:
+            atk_bonus = self.attack_value
+            hlt_bonus = self.health_value
+        self.attack_temp_boost += atk_bonus
+        self.health_temp_boost += hlt_bonus
