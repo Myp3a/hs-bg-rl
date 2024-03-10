fe5c773ef602c22b03e5a0ca57aa6d1587d02b7d
Myp3a <murza8965@gmail.com>
2024-03-10 21:37:36 +0300

    feat: mechanized gift horse

---
+++ cards/minions/mechanized_gift_horse.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from cards.minions.mechorse import Mechorse
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class MechanizedGiftHorse(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 161
+        self.classes = [MinionClass.Mech, MinionClass.Beast]
+        self.level = 5
+        self.base_attack_value = 6
+        self.base_health_value = 6
+        self.hooks["deathrattle"].append(self.summon_mechorses)
+
+    def summon_mechorses(self, position) -> None:
+        for _ in range(2):
+            h = Mechorse(self.army)
+            if self.triplet:
+                h.triplet = True
+            for hook in h.hooks["on_get"]:
+                hook()
+            self.army.add(h, position)
+            for hook in self.army.hooks["on_minion_summon"]:
+                hook(h)
---
+++ cards/minions/mechorse.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from .mechapony import Mechapony
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class Mechorse(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 162
+        self.classes = [MinionClass.Mech, MinionClass.Beast]
+        self.level = 1
+        self.base_attack_value = 2
+        self.base_health_value = 2
+        self.hooks["deathrattle"].append(self.summon_mechapony)
+
+    def summon_mechapony(self, position) -> None:
+        for _ in range(2):
+            h = Mechapony(self.army)
+            if self.triplet:
+                h.triplet = True
+            for hook in h.hooks["on_get"]:
+                hook()
+            self.army.add(h, position)
+            for hook in self.army.hooks["on_minion_summon"]:
+                hook(h)
