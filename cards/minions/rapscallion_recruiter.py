35724f6a174fd37d393a4beabc0e5567000e65e8
Myp3a <murza8965@gmail.com>
2024-03-10 22:40:23 +0300

    feat: rapscallion recruiter

---
+++ cards/minions/rapscallion_recruiter.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+from .scallywag import Scallywag
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class RapscallionRecruiter(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 167
+        self.classes = [MinionClass.Pirate]
+        self.level = 5
+        self.base_attack_value = 9
+        self.base_health_value = 3
+        self.hooks["deathrattle"].append(self.summon_scallywags)
+
+    def summon_scallywags(self, position):
+        for _ in range(3):
+            sw = Scallywag(self.army)
+            if self.triplet:
+                sw.triplet = True
+            for hook in sw.hooks["on_get"]:
+                hook()
+            self.army.add(sw, position)
