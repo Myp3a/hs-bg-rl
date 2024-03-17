b756f5b89ea9808c793690a7bc9f6e24d8143599
Myp3a <murza8965@gmail.com>
2024-03-18 00:01:01 +0300

    feat: rock rock

---
+++ cards/minions/rock_rock.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from cards.minion import Minion, MinionClass
+
+if TYPE_CHECKING:
+    from models.army import Army
+
+
+class RockRock(Minion):
+    def __init__(self, army: Army) -> None:
+        super().__init__(army)
+        self.minion_id = 202
+        self.classes = [MinionClass.Elemental]
+        self.level = 6
+        self.base_attack_value = 5
+        self.base_health_value = 5
+        self.flag_attack = True
+        self.hooks["on_turn_start"].append(self.flip_flag)
+        self.hooks["on_lose"].append(self.remove_hook)
+        self.hooks["on_play"].append(self.put_hook)
+
+    def put_hook(self) -> None:
+        self.army.hooks["on_minion_play"].append(self.on_play)
+
+    def remove_hook(self) -> None:
+        self.army.hooks["on_minion_play"].remove(self.on_play)
+
+    def flip_flag(self):
+        self.flag_attack = not self.flag_attack
+
+    def on_play(self, played):
+        if MinionClass.Elemental in played.classes:
+            if self.triplet:
+                boost = 4
+            else:
+                boost = 2
+            for t in self.army.cards:
+                if self.flag_attack:
+                    t.attack_perm_boost += boost
+                    for hook in self.army.hooks["on_values_change_perm"]:
+                        hook(t, boost, 0)
+                else:
+                    t.health_perm_boost += boost
+                    for hook in self.army.hooks["on_values_change_perm"]:
+                        hook(t, 0, boost)
