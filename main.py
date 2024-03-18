fef24b5ebb93280fd9ec6a75d1ad7252368010b5
Myp3a <murza8965@gmail.com>
2024-03-18 12:37:25 +0300

    fix: lower resource usage

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ ModelCatalog.register_custom_model("HSModel", TorchActionMaskModel)
 config = (
         PPOConfig()
         .environment(env=env_name, clip_actions=True, disable_env_checking=True)
-        .rollouts(num_rollout_workers=10, rollout_fragment_length="auto")
+        .rollouts(num_rollout_workers=5, rollout_fragment_length="auto")
         .training(
-            train_batch_size=2048,
+            train_batch_size=1024,
             lr=2e-5,
             gamma=0.99,
             lambda_=0.9,
